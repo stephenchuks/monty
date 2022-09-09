56574cb59924f1deee749055c7f1893c384badf1
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-09-09 05:35:33 +0100

    monty

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@head: stack head
+  *@counter: counter of line
+  *Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
